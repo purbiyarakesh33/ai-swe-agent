f429f564f7ec5a992b4caf17f7d50ce53f7a86d5
Rakesh <purbiyarakesh33@gmail.com>
2026-08-30 16:09:44 +0530

    Remove demo-only tests and toy repo

--- test_agent1.py
+++
@@ -1,6 +0,0 @@
-from agents.agent1_understanding import investigate
-
-result = investigate("./toy_repo", "the add function gives wrong results")
-print(result)
-
-
--- toy_repo/calculator.py
+++
@@ -1,7 +0,0 @@
-def add(a, b):
-    """Add two numbers."""
-    return a - b  # bug: should be a + b
-
-
-def subtract(a, b):
-    return a - b
